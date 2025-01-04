a27ade09356af7e7a7ec71279ffae58214e2d775
Cesar Bacca <bacca2095@gmail.com>
2025-01-03 21:47:11 -0500

    feat: agregar configuración de Nginx y exponer puertos en Docker

--- src/contabo/providers/contabo.service.ts
+++ src/contabo/providers/contabo.service.ts
@@ -179,6 +179,7 @@ export class ContaboService {
           {
             imageId: '66abf39a-ba8b-425e-a385-8eb347ceac10',
             rootPassword: serverCredential.secretId,
+            defaultUser: 'root',
           },
           {
             headers: {
@@ -264,12 +265,126 @@ export class ContaboService {
         password: serverCredential.password,
       });
 
-      const result = await connection.execCommand('ls -la', { cwd: '/' });
+      const update = await connection.execCommand('apt-get update');
 
-      console.log(result);
+      console.log(update);
+
+      const installDependencies = await connection.execCommand(
+        'apt-get install ca-certificates curl',
+      );
+
+      console.log(installDependencies);
+
+      const setPermissions = await connection.execCommand(
+        'install -m 0755 -d /etc/apt/keyrings',
+      );
+
+      console.log(setPermissions);
+
+      const addKey = await connection.execCommand(
+        'curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc',
+      );
+
+      console.log(addKey);
+
+      const chmodKey = await connection.execCommand(
+        'chmod a+r /etc/apt/keyrings/docker.asc',
+      );
+
+      console.log(chmodKey);
+
+      const addRepo = await connection.execCommand(
+        'echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null',
+      );
+
+      console.log(addRepo);
+
+      const updateDocker = await connection.execCommand('apt-get update');
+
+      console.log(updateDocker);
+
+      const installDocker = await connection.execCommand(
+        'apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nginx',
+      );
+
+      console.log(installDocker);
+
+      const copyFile = await connection.putFile(
+        'docker-compose.yml',
+        '/root/docker-compose.yml',
+      );
+
+      console.log(copyFile);
+
+      const dockerCompose = await connection.execCommand(
+        'docker compose up -d',
+      );
+
+      console.log(dockerCompose);
+
+      const enableNginx = await connection.execCommand(
+        'systemctl enable nginx',
+      );
+      const startNginx = await connection.execCommand('systemctl start nginx');
+      const statusNginx = await connection.execCommand(
+        'systemctl status nginx',
+      );
+
+      console.log({ enableNginx, startNginx, statusNginx });
+
+      const ufw = await connection.execCommand(
+        "ufw allow 'Nginx Full' && ufw reload",
+      );
+
+      console.log(ufw);
+
+      const makeDir = await connection.execCommand(
+        'sudo mkdir -p /etc/ssl/self-signed',
+      );
+
+      console.log(makeDir);
+
+      const generateCert = await connection.execCommand(
+        'openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/self-signed/selfsigned.key -out /etc/ssl/self-signed/selfsigned.crt',
+      );
+
+      console.log(generateCert);
+
+      const dhparam = await connection.execCommand(
+        'openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048',
+      );
+
+      console.log(dhparam);
+
+      const copyNginxConfig = await connection.putFile(
+        'app',
+        '/etc/nginx/sites-available/app',
+      );
+
+      console.log(copyNginxConfig);
+
+      const symlink = await connection.execCommand(
+        'ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/',
+      );
+
+      console.log(symlink);
+
+      const testNginx = await connection.execCommand('nginx -t');
+
+      console.log(testNginx);
+
+      const reloadNginx = await connection.execCommand(
+        'systemctl restart nginx',
+      );
+
+      console.log(reloadNginx);
 
       return serverResponse.data;
     } catch (error) {
+      console.log(error);
       if (error instanceof AxiosError) {
         throw new HttpException(
           error.response?.data || 'Failed to fetch server list',
